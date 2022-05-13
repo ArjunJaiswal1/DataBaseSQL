a64ef07968047cb057b1177a9be539eb508b4fca
ArjunJaiswal1 <97870068+ArjunJaiswal1@users.noreply.github.com>
2022-05-13 23:48:23 +0530

    sql

---
+++ sql.sql
@@ -0,0 +1,13 @@
+create database BRP;
+show databases;
+use BRP;
+drop database brp;
+CREATE TABLE employee_table(  
+    id int NOT NULL AUTO_INCREMENT,  
+    name varchar(45) NOT NULL,  
+    occupation varchar(35) NOT NULL,  
+    age int NOT NULL,  
+    PRIMARY KEY (id)  
+    );
+    show tables;
+    
